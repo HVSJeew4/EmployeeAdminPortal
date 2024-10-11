21f00bc0fdb05a5591098e1b9479c1e6a402190b
Subhash Jeewantha <126590375+HVSJeew4@users.noreply.github.com>
2024-10-11 12:45:36 +0530

    GetMethodCompleted

---
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -0,0 +1,26 @@
+﻿using EmployeeAdminPortal.Data;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeeAdminPortal.Controllers
+{
+    //localhost:xxx/api/employees
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeesController : ControllerBase
+    {
+        private readonly ApplicationDbContext _dbContext;
+
+        public EmployeesController(ApplicationDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+        [HttpGet]
+        public IActionResult GetAllEmployees()
+        {
+            var allEmployees=_dbContext.Employees.ToList();
+
+            return Ok(allEmployees);
+        }
+    }
+}
