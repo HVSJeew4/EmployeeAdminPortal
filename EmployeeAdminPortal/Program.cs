37efc5cf7b66141a083312071ad5f0b6c6aa8525
Subhash Jeewantha <126590375+HVSJeew4@users.noreply.github.com>
2024-10-11 11:33:07 +0530

    UptoMigrationsCompleted

--- EmployeeAdminPortal/Program.cs
+++ EmployeeAdminPortal/Program.cs
@@ -1,4 +1,9 @@
+using EmployeeAdminPortal.Data;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+
 
 // Add services to the container.
 
@@ -6,6 +11,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+options.UseSqlServer(builder.Configuration.GetConnectionString("DefulatConnection")));
 
 var app = builder.Build();
 
