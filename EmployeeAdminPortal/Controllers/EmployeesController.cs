fd5cf21bb4c5ca717760dbe09bbcf988d310365e
Subhash Jeewantha <126590375+HVSJeew4@users.noreply.github.com>
2024-10-11 13:40:07 +0530

    EmployeeDtoCompleted

--- EmployeeAdminPortal/Controllers/EmployeesController.cs
+++ EmployeeAdminPortal/Controllers/EmployeesController.cs
@@ -1,6 +1,9 @@
 ﻿using EmployeeAdminPortal.Data;
+using EmployeeAdminPortal.Models;
+using EmployeeAdminPortal.Data.Entities;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Numerics;
 
 namespace EmployeeAdminPortal.Controllers
 {
@@ -18,9 +21,38 @@ namespace EmployeeAdminPortal.Controllers
         [HttpGet]
         public IActionResult GetAllEmployees()
         {
-            var allEmployees=_dbContext.Employees.ToList();
+            var allEmployees = _dbContext.Employees.ToList();
 
             return Ok(allEmployees);
+        }
+
+        [HttpGet]
+        [Route("{id:guid}")]
+        public IActionResult GetEmployeeById(Guid id) 
+        {
+            var employee = _dbContext.Employees.Find(id);
+            if (employee == null)
+            {
+                return NotFound();
+            }
+            return Ok(employee);
+        }
+
+        [HttpPost]
+        public IActionResult AddEmployee(AddEmployeeDto addEmployeeDto)
+        {
+            var employeeEntity = new Employee
+            {
+                Name = addEmployeeDto.Name,
+                Email = addEmployeeDto.Email,
+                Phone = addEmployeeDto.Phone,
+                Salary = addEmployeeDto.Salary
+            };
+
+            _dbContext.Employees.Add(employeeEntity);
+            _dbContext.SaveChanges();
+
+            return Ok(employeeEntity);
         }
     }
 }
